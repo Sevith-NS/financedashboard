2951adc6fff8c0ac5f204a42908fa903a0b07e44
Sevith-55 <sevith.sadashiva@bca.christuniversity.in>
2024-05-31 19:10:49 +0530

    theme change

--- client/src/components/DashboardBox.tsx
+++ client/src/components/DashboardBox.tsx
@@ -3,9 +3,9 @@ import { styled } from "@mui/material";
 
 //eslint-disable-next-line
 const DashboardBox = styled(Box)(({}) =>({
-        backgroundColor: "#2d2d34",
+        backgroundColor: "#000",
         borderRadius: "1rem",
-        boxShadow: "0.15rem 0.2rem 0.15rem 0.1rem rgba(0, 0, 0, .8)"
+        boxShadow: "0.15rem 0.2rem 0.15rem 0.1rem rgba(88, 88, 88, .8)"
 }));
 
 export default DashboardBox;
